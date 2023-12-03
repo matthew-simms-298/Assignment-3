dabcf7f3c185ef27c8e00010e6fa3005ba30156f
Ryan Clarkson <119633468+RCC-STUDIO@users.noreply.github.com>
2023-12-03 15:38:35 -0700

    Update Footer.jsx

--- movie-auth/src/app/components/Footer.jsx
+++ movie-auth/src/app/components/Footer.jsx
@@ -4,8 +4,34 @@ import React from "react"
 
 export default function Footer() {
     return (
-        <div className="flex flex-col">
-            Testing For Footer
+        <footer className="flex items-center justify-between h-10 mb-10">
+        <div className="flex flex-row flex-wrap text-md">
+            <div className="p-5">
+                <p className="font-bold text-2xl">Internet Movie Rentals Co.</p>
+                <p className="text-s">© 2012-2024</p>
+            </div>
+            <div className="p-5">
+                <div>
+                    <p className="font-bold">Email:</p>
+                </div>
+                <div>
+                    <p className="underline">internetmovies.rental@imr.com</p>
+                </div>
+            </div>
+            <div className="p-5">
+                <div>
+                    <p className="font-bold">Base of Operation:</p>
+                </div>
+                <div>
+                    <p>Movies ST BLVD NW. Los Angeles CA, United States</p>
+                </div>
+            </div>
+            <div className="p-5">
+                <div>
+                    <p className="font-bold">RCC-STUDIO Socials:</p>
+                </div>
+            </div>
         </div>
+        </footer> 
     )
 }
