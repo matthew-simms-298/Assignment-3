106a0b69ef13f4743706ed51407d9e850497b438
Ryan Clarkson <119633468+RCC-STUDIO@users.noreply.github.com>
2023-12-03 11:02:33 -0700

    Update .gitingnore and route.js

--- movie-auth/src/app/api/auth/[...nextauth]/route.js
+++ movie-auth/src/app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,16 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
-    {/* This is where you put the Auth Provider, A local .env / .env.local (Enviroment file is required to run this code) */},
+    {/* 
+    This is where you put the Auth Provider, A .env / .env.local (Enviroment file is required to run this code)
+    The .env file should contain the following:
+    CLIENT_ID=YOUR_CLIENT_ID
+    CLIENT_SECRET=YOUR_CLIENT_SECRET
+    NEXTAUTH_URL=http://localhost:3000
+    NEXTAUTH_SECRET=YOUR_SECRET
+
+    The Providers and Documentation are listed here: https://next-auth.js.org/providers
+    */},
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
