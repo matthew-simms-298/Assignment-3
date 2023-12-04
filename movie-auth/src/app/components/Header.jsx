118554e3e455167e29616e85462ab71d61b87cf6
Ryan Clarkson <119633468+RCC-STUDIO@users.noreply.github.com>
2023-12-03 23:51:59 -0700

    Update File
    "Whooooooo Yeaaahhh" - Jamie Oliver
    
    TimeStamp: 11:51pm

--- movie-auth/src/app/components/Header.jsx
+++ movie-auth/src/app/components/Header.jsx
@@ -9,29 +9,36 @@ export default function Header() {
     return (
         <header>
             <nav>
-                <div className="flex flex-row bg-white dark:bg-gray-800 dark:text-white text-slate-900 py-5 px-3">
+                <div className="flex flex-row bg-white dark:bg-gray-800 dark:text-white text-slate-900 py-5 px-3 border-y">
                     <div>
-                        <a href="/" className="px-3 mr-2 p-3
-                        rounded hover:bg-blue-400 dark:hover:bg-blue-700">Internet Movies Rental Home</a>
+                        <img src="\images\logo.png" 
+                        alt="Company logo of a movie clip sign with the company name on it"
+                        className='h-22 w-22 pb-1 mr-2'/>
                     </div>
+                    <div className='pt-10'>
+                        <a href="/" className="px-3 mr-2 p-3
+                        rounded hover:bg-blue-400 dark:hover:bg-blue-700 text-2xl">Home</a>
+                    </div>
+                    {/*
                     {status === 'loading' && (
-                    <div>
-                        <p>Loading...</p>
+                    <div className='pt-10'>
+                        <p className='rounded px-3 mr-2 p-3 text-2xl'>Loading...</p>
                     </div>
                     )}
+                    */}
                     {status === 'authenticated' && (
                         <>
-                            <div>
+                            <div className='pt-10'>
                                 <a href="/signout" className="px-3 mr-2 p-3
-                                rounded hover:bg-blue-400 dark:hover:bg-blue-700" >Sign Out</a>
+                                rounded hover:bg-blue-400 dark:hover:bg-blue-700 text-2xl">Sign Out</a>
                                 <p className="px-3 mr-2" >{session.user.name}</p>
                             </div>
                         </>
                     )}
                     {status === 'unauthenticated' && (
-                        <div>
+                        <div className='pt-10'>
                             <a href="/signin" className="px-3 mr-2 p-3
-                            rounded hover:bg-blue-400 dark:hover:bg-blue-700">Sign In</a>
+                            rounded hover:bg-blue-400 dark:hover:bg-blue-700 text-2xl">Sign In</a>
                         </div>
                     )}
                 </div>
